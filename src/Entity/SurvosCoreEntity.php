8a1f46f95101e1f876800c5966909249ae8059dd
Tac Tacelosky <tacman@gmail.com>
2022-09-14 06:26:18 -0500

    add deprecated base entities for migrations

---
+++ src/Entity/SurvosCoreEntity.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Survos\CoreBundle\Entity;
+
+use Survos\CoreBundle\Entity\RouteParametersInterface;
+use Survos\CoreBundle\Entity\RouteParametersTrait;
+use Symfony\Component\PropertyAccess\Exception\InvalidArgumentException;
+use Symfony\Component\PropertyAccess\Exception\NoSuchPropertyException;
+use Symfony\Component\PropertyAccess\PropertyAccess;
+use function Symfony\Component\String\u;
+
+/**
+ * @deprecated implement RouteParametersInterface instead.
+ */
+abstract class SurvosCoreEntity implements RouteParametersInterface
+{
+    use RouteParametersTrait;
+
+}
