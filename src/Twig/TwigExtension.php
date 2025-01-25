79af2679b25ae1f1bdae66571edb1736b557c59a
tacman <info@survos-ci.com>
2025-01-25 13:01:03 +0000

    fixed route, remove dump

--- src/Twig/TwigExtension.php
+++ src/Twig/TwigExtension.php
@@ -14,7 +14,7 @@ class TwigExtension extends AbstractExtension
             // If your filter generates SAFE HTML, add ['is_safe' => ['html']]
             // Reference: https://twig.symfony.com/doc/3.x/advanced.html#automatic-escaping
             new TwigFilter('get_class', fn (object $obj) => $obj::class),
-            new TwigFilter('json_pretty', fn (object|array $obj) => json_encode($obj, JSON_PRETTY_PRINT+JSON_UNESCAPED_SLASHES)),
+            new TwigFilter('json_pretty', fn (object|array|null $obj) => json_encode($obj, JSON_PRETTY_PRINT+JSON_UNESCAPED_SLASHES)),
             new TwigFilter('short_class', fn (object $obj) => (new \ReflectionClass($obj))->getShortName()),
         ];
     }
