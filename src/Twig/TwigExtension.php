468332bcb862b8943f9bb6d5f215939cdf5c5bbf
tacman <info@survos-ci.com>
2025-02-20 13:35:55 +0000

    enhance json_pretty

--- src/Twig/TwigExtension.php
+++ src/Twig/TwigExtension.php
@@ -14,7 +14,8 @@ class TwigExtension extends AbstractExtension
             // If your filter generates SAFE HTML, add ['is_safe' => ['html']]
             // Reference: https://twig.symfony.com/doc/3.x/advanced.html#automatic-escaping
             new TwigFilter('get_class', fn (object $obj) => $obj::class),
-            new TwigFilter('json_pretty', fn (object|array|null $obj) => json_encode($obj, JSON_PRETTY_PRINT+JSON_UNESCAPED_SLASHES)),
+            new TwigFilter('json_pretty', $this->json_pretty(...), ['is_safe' => ['html']]),
+
             new TwigFilter('short_class', fn (object $obj) => (new \ReflectionClass($obj))->getShortName()),
         ];
     }
@@ -25,4 +26,20 @@ class TwigExtension extends AbstractExtension
 //            new TwigFunction('function_name', [::class, 'doSomething']),
         ];
     }
+
+    public function json_pretty(object|array|null $value, ?string $wrapper='pre', array $wrapperAttributes=[]): string
+    {
+        if (is_string($value)) {
+            $data = json_decode($value);
+        } else {
+            $data = $value;
+        }
+        $json = json_encode($data, JSON_PRETTY_PRINT | JSON_UNESCAPED_SLASHES | JSON_UNESCAPED_UNICODE);
+        if ($wrapper) {
+            // @todo: attributes
+            $json = sprintf("<%s>%s</%s>", $wrapper, $json, $wrapper);
+        }
+        return $json;
+    }
+
 }
